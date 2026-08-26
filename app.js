84b7eb3b65df4b4b4dea902fce764adae0ec77e4
mavurioficial <mavuri.oficial@gmail.com>
2026-08-26 19:09:03 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+document.getElementById("testar").addEventListener("click", async () => {
+
+  const resultado = document.getElementById("resultado");
+
+  resultado.textContent = "Consultando a API do Mercado Livre...";
+
+  try {
+
+    const response = await fetch(
+      "https://api.mercadolibre.com/sites/MLB/search?q=iphone"
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Erro HTTP: ${response.status}`
+      );
+    }
+
+    const data = await response.json();
+
+    resultado.textContent =
+      JSON.stringify(data, null, 2);
+
+  } catch (erro) {
+
+    resultado.textContent =
+      "ERRO AO CONSULTAR API:\n\n" + erro.message;
+
+  }
+
+});
